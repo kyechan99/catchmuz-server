61c060894be4c95fe3f0cd4aab70bae20bd046b3
kyechan99 <kyechan99@gmail.com>
2021-07-16 21:11:10 +0900

    ADD : Send song data to Client & Make match the song

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const server = require('http').createServer(app);
 
 const ROOM_CODE = 'ROOM_';
 
+const songData = require('./song/song_kr.json');
+
+console.log(songData);
+
+console.log(songData.filter((song, idx) => {
+  return (song.tags.includes('TOP100'));
+}))
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -27,18 +35,18 @@ let roomData = {
     userList: [
       {
         socketId: '',
-        userName: '',
-        userProfile: 1,
-        userColor: 0
+        nickname: '',
+        profile: 1,
+        color: 0
       }
     ],
+    songList: [],
     isPlaying: false
   }
 
 };
 
 let roomIdx = 0;
-
 
 
 io.on('connection', (socket) => {
@@ -66,36 +74,42 @@ io.on('connection', (socket) => {
           profile: data.user.profile,
           color: 0
         }
-      ]
+      ],
+      songList: [],
+      isPlaying: false
     };
 
     socket.join(ROOM_CODE + roomIdx);
-    socket.emit('get room code', { roomCode: roomIdx })
+    socket.emit('get room code', { roomCode: roomIdx });
   });
 
   //================================
   // Room - Join
   socket.on('join room', (data) => {
     console.log('JOIN ROOM', roomData[data.roomCode], data.user.socketId);
-    console.log('RR', roomData[data.roomCode].userList.filter(e => e.socketId === data.user.socketId).length);
 
     // 존재하는 방인지
-    if (roomData[data.roomCode]) {
+    if (roomData[data.roomCode] != undefined) {
       // 해당 방에 내가 이미 들어와 있는지
-      if (roomData[data.roomCode].userList.filter(e => e.socketId === data.user.socketId).length > 0) {
-        // 방장임
-      } else {
-        /// 방장이 아님
+      if (roomData[data.roomCode].userList.length > 0) {
+        if (roomData[data.roomCode].userList.filter(e => e.socketId === data.user.socketId).length > 0) {
+          // 방장임
+          socket.emit('your manager');
+        } else {
+          /// 방장이 아님
 
-        // 기존에 있던 유저들에게 내 데이터를 보내줌
-        io.to(ROOM_CODE + data.roomCode).emit('someone join', data.user);
+          /// TODO : 인원수 체크
 
-        // 나에게 전체 유저 데이터들을 보내줌
-        socket.emit('join room', roomData[data.roomCode].userList);
+          // 기존에 있던 유저들에게 내 데이터를 보내줌
+          io.to(ROOM_CODE + data.roomCode).emit('someone join', data.user);
 
-        roomData[data.roomCode].userList.push(data.user);
+          // 나에게 전체 기존 유저 데이터들을 보내줌
+          socket.emit('join room', roomData[data.roomCode].userList);
 
-        socket.join(ROOM_CODE + data.roomCode);
+          roomData[data.roomCode].userList.push(data.user);
+
+          socket.join(ROOM_CODE + data.roomCode);
+        }
       }
     } else {
       // error : 없는 방
@@ -105,8 +119,10 @@ io.on('connection', (socket) => {
   //================================
   // Room - Exit
   socket.on('exit room', (data) => {
-
     socket.leave(ROOM_CODE + data.roomCode);
+
+    if (!roomData[data.roomCode])
+      return;
 
     // 인원수가 1이라면 나 혼자였던 방. 이제 내가 나갔으니 방을 삭제
     if (roomData[data.roomCode].userList.length <= 1) {
@@ -141,10 +157,58 @@ io.on('connection', (socket) => {
 
   //================================
   // Room - Send Chat
-  socket.on('send chat', data => {    
+  socket.on('send chat', data => {
+
+    data.isAnswer = false;
+    if (roomData[data.roomCode].isPlaying) {
+      // Check is Answer
+      if (roomData[data.roomCode].songList[0].answer.includes( data.msg.replace(/\s/g, '') )) {
+        data.isAnswer = true; 
+        roomData[data.roomCode].songList.shift();
+      }
+    }
+
     io.to(ROOM_CODE + data.roomCode).emit('receive chat', data);
 
+    // if (data.isAnswer) {
+    //   nextSong(data.roomCode);
+    // }
+
   });
+
+  socket.on('request next', data => {
+    console.log('REQUEST NEXT SONG');
+
+    if (roomData[data.roomCode] && roomData[data.roomCode].isPlaying) {
+      nextSong(data.roomCode);
+    }
+  })
+
+  function nextSong(roomCode) {
+    if (roomData[roomCode].songList.length > 0) {
+      io.to(ROOM_CODE + roomCode).emit('next song', roomData[roomCode].songList[0]);
+    } else {
+      roomData[roomCode].isPlaying = false;
+      console.log('ERROR : No song found');
+      io.to(ROOM_CODE + roomCode).emit('game end');
+    }
+  }
+
+  //================================
+  // Game - Game Start
+  socket.on('game start', (data) => {
+    console.log('GAME START !!!!!!!!!');
+    roomData[data.roomCode].isPlaying = true;
+    console.log(roomData[data.roomCode]);
+    roomData[data.roomCode].songList = songData.filter((song, idx) => {
+      return (song.tags.includes('TOP100'));
+    });
+
+    io.to(ROOM_CODE + data.roomCode).emit('game start', data);
+    nextSong(data.roomCode);
+  });
+
+
 
 });
 
